f2114771807d4010ab4d1f021a26a613d8742bde
NaotoCoding <naoto.zaniah@gmail.com>
2024-06-08 15:06:22 +0900

    generateコマンドで生成されるファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,10 @@ module TurboTasks
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets       false
+      g.helper       false
+      g.system_tests nil
+    end
   end
 end
