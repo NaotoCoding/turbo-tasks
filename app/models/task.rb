022aa5af6d81e0e07229672ffa165ab749661948
NaotoCoding <naoto.zaniah@gmail.com>
2024-06-08 18:33:40 +0900

    exnumに合わせてenum設定を修正し、status用の翻訳を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,5 +11,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :status, presence: true
-  enum :status, { have_not_done_yet: 10, completed: 20 }
+  exnum status: { have_not_done_yet: 10, completed: 20 }
 end
