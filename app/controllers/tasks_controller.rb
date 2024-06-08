5382b67f1d5b20abd87724d423e3e4d49e697650
NaotoCoding <naoto.zaniah@gmail.com>
2024-06-08 18:57:19 +0900

    タスクをタスク一覧画面から登録できるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,22 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
     @tasks = Task.all
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.have_not_done_yet!
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -7,4 +7,39 @@ RSpec.describe "Tasks", type: :request do
       expect(response).to have_http_status 200
     end
   end
+
+  describe "POST /tasks" do
+    subject { post tasks_path, params: }
+
+    context "パラメータが正常な場合" do
+      let(:params) { { task: { title: "タイトル" } } }
+
+      it "タスクが作成される" do
+        expect { subject }.to change(Task, :count).by(1)
+      end
+
+      it "タスクのステータスがhave_not_done_yetとして作成される" do
+        subject
+        expect(Task.last.have_not_done_yet?).to be true
+      end
+
+      it "タスク一覧画面にリダイレクトする" do
+        subject
+        expect(response).to redirect_to tasks_path
+      end
+    end
+
+    context "パラメータが異常な場合" do
+      let(:params) { { task: { title: "" } } }
+
+      it "タスクが作成されない" do
+        expect { subject }.not_to(change(Task, :count))
+      end
+
+      it "422を返す" do
+        subject
+        expect(response).to have_http_status 422
+      end
+    end
+  end
 end
