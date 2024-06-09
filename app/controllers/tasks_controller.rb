60e8eab67f5ea4dbdcce414aa8d068eb50c47ee5
NaotoCoding <naoto.zaniah@gmail.com>
2024-06-09 22:34:01 +0900

    タスクの削除を作成(with turbo_stream)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,12 @@ class TasksController < ApplicationController
     end
   end
 
+  # TODO: request spec記述
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy!
+  end
+
   private
 
     def set_tasks
